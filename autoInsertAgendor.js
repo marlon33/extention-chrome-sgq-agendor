190ac0ce966d6d2052fe5312b7b0e89ce19b5370
oMarlon <41172965+marlon33@users.noreply.github.com>
2024-04-03 16:06:41 -0300

    fix: cookie to local storage

--- autoInsertAgendor.js
+++ autoInsertAgendor.js
@@ -151,24 +151,20 @@ function checkOrganization(orgSGQ, orgAgendor) {
     }
 }
 
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+function setLocalStorageItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
 }
 
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+function getLocalStorageItem(key) {
+    var value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+}
+
+function removLocalStorageItem(key) {
+    localStorage.removeItem(key);
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
 }
 
