0ddba765eb31f89a6c005ff72c04cd885205c7ca
oMarlon <41172965+marlon33@users.noreply.github.com>
2024-04-03 21:52:38 -0300

    update

--- autoInsertAgendor.js
+++ autoInsertAgendor.js
@@ -3,7 +3,8 @@
 // dealStages
 // id: 3000255
 
-const url = 'https://sendagendor.olivehill.com.br/';
+const url = 'https://36a6-168-228-217-148.ngrok-free.app/';
+// const url = 'https://sendagendor.olivehill.com.br/';
 const token = '419a935e-22ec-46aa-b42e-0ab293225556';
 
 let infosHeader = document.querySelector("#orcamento_form > div.col-sm-9.form-fields > div > div > fieldset:nth-child(1) > div.field-boxes.row > div:nth-child(1) > div > p")
@@ -22,17 +23,21 @@ let servicePrice = findInfo(arrInfosHeader, "Valor total deste orçamento: R$ ")
 
 let data = {};
 
+const userId = getLocalStorageItem('userId');
+const funnelIdLS = getLocalStorageItem('funnelId');
+const cardIdLS = getLocalStorageItem('cardId');
+const cardSequenceLS = getLocalStorageItem('cardSequence');
+
+let funnelDealStages = [];
+
 if (budget) {
-    // console.log(`${budget.string}`);
     data.budget = budget.string;
 }
 if (clientName) {
-    // console.log(`${clientName.string}`);
     data.clientName = clientName.string;
 }
 if (servicePrice) {
     let price = servicePrice.string.replace(",00", "").replace(".", "");
-    // console.log(`${price}`);
     data.servicePrice = price;
 }
 
@@ -40,7 +45,9 @@ insertEl(data)
 action()
 getMe()
 getFunnel()
-
+function isNumber(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+}
 function getMe() {
     const route = 'api/me/';
 
@@ -53,7 +60,8 @@ function getMe() {
         })
         .then(response => {
             console.log(response)
-            setLocalStorageItem('user', {id:response.accountId,name:response.name});
+            setLocalStorageItem('userId', response.data.accountId);
+            setLocalStorageItem('user', { id: response.data.accountId, name: response.data.name });
         })
         .catch(error => {
             console.error('There was a problem with the fetch request:', error);
@@ -72,23 +80,69 @@ function getFunnel() {
         })
         .then(response => {
             console.log(response)
-            // setLocalStorageItem('user', {id:response.accountId,name:response.name});
-            
             const res = document.querySelector("#setFunnel");
-            console.log(res, response)
             let options = '';
+
             response.data.forEach(el => {
-                // if (checkOrganization(data.clientName, el.name)) {
+                if (funnelIdLS == el.id) {
                     options += `<option value="${el.id}" selected>${el.name}</option>`;
-                // } else {
-                //     options += `<option value="${el.id}">${el.name}</option>`;
-                // }
+                } else {
+                    options += `<option value="${el.id}">${el.name}</option>`;
+                }
+
+                funnelDealStages.push({ "funnelId": el.id, "dealstages": el.dealStages })
             });
-            res.innerHTML = `<select id="organizationId" style="width: 100%;">${options}</select>`
+            res.innerHTML = `<select id="funnelId" style="width: 100%;">
+                <option>Selecione um funil</option>
+                ${options}
+            </select>`;
+
+            const selectFunnelCard = document.querySelector("#funnelId");
+            if (selectFunnelCard) {
+                if(funnelIdLS != null){
+                    setCardElement(funnelIdLS);
+                }
+                selectFunnelCard.addEventListener('change', function (e) {
+                    const funnelId = e.target.value;
+                    setCardElement(funnelId);
+                });
+            }
+
+
         })
         .catch(error => {
             console.error('There was a problem with the fetch request:', error);
         });
+}
+
+function setCardElement(funnelId){
+    const setCard = document.querySelector("#setCard");
+    let optionsCard = '';
+    setLocalStorageItem('funnelId', funnelId);
+    funnelDealStages.forEach(fds => {
+        if (fds.funnelId == funnelId) {
+            fds.dealstages.forEach(fdsds => {
+                if (cardIdLS == fdsds.id) {
+                    optionsCard += `<option data-sequence="${fdsds.sequence}" value="${fdsds.id}" selected>${fdsds.name}</option>`;
+                } else {
+                    optionsCard += `<option data-sequence="${fdsds.sequence}"  value="${fdsds.id}">${fdsds.name}</option>`;
+                }
+            });
+        }
+    }); 
+    
+    setCard.innerHTML = `<select id="cardId" style="width: 100%;">
+            <option>Selecione um card</option>
+            ${optionsCard}
+        </select>`;
+
+    const selectCard = document.querySelector("#cardId");
+    if (selectCard) {
+        selectCard.addEventListener('change', function (el) {
+            setLocalStorageItem('cardId', el.target.value);
+            setLocalStorageItem('cardSequence', el.target.selectedOptions[0].dataset.sequence);
+        })
+    }
 }
 
 function action() {
@@ -103,7 +157,6 @@ function action() {
         })
         .then(response => {
             const res = document.querySelector("#selecOrganization");
-            console.log(res, response)
             let options = '';
             response.data.forEach(el => {
                 if (checkOrganization(data.clientName, el.name)) {
@@ -113,7 +166,10 @@ function action() {
                 }
             });
             selectCard = ``
-            res.innerHTML = `<select id="organizationId" style="width: 100%;">${options}</select>`
+            res.innerHTML = `<select id="organizationId" style="width: 100%;">
+                <option>Selecione uma empresa</option>
+                ${options}
+            </select>`
         })
         .catch(error => {
             console.error('There was a problem with the fetch request:', error);
@@ -122,43 +178,55 @@ function action() {
 
 function insertEl(data) {
     document.querySelector("#orcamento_form > div.col-sm-3.form-buttons > div > div").insertAdjacentHTML("afterend", `
-<div style="display: flex;">
-    <div>Empresa:</div>
-    <div id="setFunnel" style="width: 100%;"></div>
-</div>
-    <div>
-    <div style="display: flex;">
-    <div>Titulo:</div>
-    <div style="width: 100%;">
-        <input style="width: 100%;" type="text" id="cardName" value="${data.clientName} - ${data.budget}">
-    </div>
-</div>
+        <div style="display: flex;">
+            <div>Funil:</div>
+            <div id="setFunnel" style="width: 100%;"></div>
+        </div>
+        
+        <div style="display: flex;">
+            <div>Card:</div>
+            <div id="setCard" style="width: 100%;"></div>
+        </div>
 
-<div style="display: flex;">
-    <div>Valor:</div>
-    <div style="width: 100%;">
-        <input style="width: 100%;" type="text" id="price" value="${data.servicePrice}">
-    </div>
-</div>
-<div style="display: flex;">
-    <div>Empresa:</div>
-    <div id="selecOrganization" style="width: 100%;"></div>
-</div>
+        <div>
+            <div style="display: flex;">
+                <div>Titulo:</div>
+                <div style="width: 100%;">
+                    <input style="width: 100%;" type="text" id="cardName" value="${data.clientName} - ${data.budget}">
+                </div>
+            </div>
 
-<div style="display: flex; margin:10px 0;" id="resSendAgendor"></div>
+            <div style="display: flex;">
+                <div>Valor:</div>
+                <div style="width: 100%;">
+                    <input style="width: 100%;" type="text" id="price" value="${data.servicePrice}">
+                </div>
+            </div>
 
-<div style="display: flex;">
-    <a onclick="sendAgendor()" style="width: 100%;margin-top: 15px;text-align: left;border: 1px solid #ccc;background: #066914;color: #fff;padding: 7px 15px;" id="saveAgendor"><span class="glyphicon glyphicon-plus"></span> Salvar Agendor</a>
-</div>
-    
-    </div>`)
+            <div style="display: flex;">
+                <div>Empresa:</div>
+                <div id="selecOrganization" style="width: 100%;"></div>
+            </div>
+
+            <div style="display: flex; margin:10px 0;" id="resSendAgendor"></div>
+
+            <div style="display: flex;">
+                <a id="btnSendToAgendor"
+                    style="width: 100%;margin-top: 15px;text-align: left;border: 1px solid #ccc;background: #066914;color: #fff;padding: 7px 15px;"
+                    id="saveAgendor"><span class="glyphicon glyphicon-plus"></span> Salvar Agendor</a>
+            </div>
+        </div>
+    `);
+
+    const btnSendToAgendor = document.querySelector("#btnSendToAgendor");
+    if (btnSendToAgendor) {
+        btnSendToAgendor.addEventListener('click', function (el) {
+            sendAgendor();
+        })
+    }
 }
 
-function resSendAgendor(text) {
-    const el = document.querySelector("#resSendAgendor");
-    el.innerHTML = `${text}`;
-    console.log(el)
-}
+function resSendAgendor(text) { document.querySelector("#resSendAgendor").innerHTML = `${text}`; }
 
 function sendAgendor() {
     const myHeaders = new Headers();
@@ -167,13 +235,13 @@ function sendAgendor() {
     const raw = JSON.stringify({
         "title": document.querySelector("#cardName").value,
         "dealStatusText": "ongoing",
-        "ownerUser": 667894,
-        "funnel": 732836,
-        "dealStage": 2,
-        "value": document.querySelector("#price").value,
+        "ownerUser": userId,
+        "funnel": parseInt(funnelIdLS),
+        "dealStage": parseInt(cardSequenceLS),
+        "value": document.querySelector("#price").value.replace(",","."),
         "allowToAllUsers": true
     });
-
+    
     const requestOptions = {
         method: "POST",
         headers: myHeaders,
@@ -181,23 +249,23 @@ function sendAgendor() {
         redirect: "follow"
     };
 
-    // const url = 'https://6795-168-228-217-148.ngrok-free.app/';
     const route = 'api/organizations/' + document.querySelector("#organizationId").value + '/';
-    // const token = '419a935e-22ec-46aa-b42e-0ab293225556';
 
-    fetch(`${url}${route}${token}`, requestOptions)
-        // fetch("https://api.agendor.com.br/v3/organizations/"+document.querySelector("#organizationId").value+"/deals", requestOptions)
+    if( isNumber(document.querySelector("#organizationId").value) && parseInt(funnelIdLS) != null && parseInt(cardSequenceLS) != null){
+        fetch(`${url}${route}${token}`, requestOptions)
         .then((response) => response.json())
         .then((result) => {
-            if(result.errors){
-                console.log("error")
+            if (result.errors) {
                 resSendAgendor(result.errors[0])
-            }else{
+            } else {
                 resSendAgendor("Enviado com sucesso!")
             }
-            console.log(result);
         })
         .catch((error) => console.error(error));
+    }else{
+        resSendAgendor("Deve preencher todos os campos corretamente!")
+    }
+
 }
 
 function checkOrganization(orgSGQ, orgAgendor) {
